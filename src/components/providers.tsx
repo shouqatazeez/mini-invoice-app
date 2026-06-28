e109b2467f8e0c77e1111d64166331b09edb3bcf
Mohammad Shouqat Azeez <mdshouqatazeez@gmail.com>
2026-06-28 20:09:24 +0530

    feat(ui): install shadcn alert-dialog, select, sonner, textarea components

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -3,12 +3,16 @@
 import { ThemeProvider } from "next-themes";
 import { SessionProvider } from "next-auth/react";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "@/components/ui/sonner";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        <TooltipProvider>{children}</TooltipProvider>
+        <TooltipProvider>
+          {children}
+          <Toaster richColors position="top-right" />
+        </TooltipProvider>
       </ThemeProvider>
     </SessionProvider>
   );
